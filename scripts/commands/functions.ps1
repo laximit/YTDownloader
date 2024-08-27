79f88d2f1a17bdf0d5beefd8afcee40676b3eca9
Murilo Lobos <murilolobos48@gmail.com>
2024-08-27 13:25:59 -0300

    changed 'yt-dlp' bin command to access 'yt-dlp.exe' file

--- scripts/commands/functions.ps1
+++ scripts/commands/functions.ps1
@@ -10,15 +10,15 @@ function FolderChoose {
 
 function ExportMP4 {
     $Global:videoLink = $textBox.Text
-    yt-dlp "$Global:videoLink" -P $Global:chosenFolder -S res,ext:mp4:m4a --recode mp4
+    & ./yt-dlp.exe "$Global:videoLink" -P $Global:chosenFolder -S res,ext:mp4:m4a --recode mp4
 }
 
 function ExportMP3 {
     $Global:videoLink = $textBox.Text
-    yt-dlp "$Global:videoLink" -f ba --extract-audio --audio-format mp3 -P $Global:chosenFolder
+    & ./yt-dlp.exe "$Global:videoLink" -f ba --extract-audio --audio-format mp3 -P $Global:chosenFolder
 }
 
 function ExportWAV {
     $Global:videoLink = $textBox.Text
-    yt-dlp "$Global:videoLink" -f ba --extract-audio --audio-format wav -P $Global:chosenFolder
+    & ./yt-dlp.exe "$Global:videoLink" -f ba --extract-audio --audio-format wav -P $Global:chosenFolder
 }
